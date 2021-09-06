c2ec1fa9428ac685725cb12d9a8fa683d1e3dd25
itssamcuz <samuel.wardpickering@gmail.com>
2021-09-06 15:13:21 +1200

    First commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// const canvas = document.getElementById("drawpad");
+
+window.addEventListener("load", () => {
+    
+const canvas = document.querySelector("#drawpad")
+const ctx = canvas.getContext ("2d")
+
+canvas.height = window.innerHeight
+canvas.width = window.innerWidth
+
+let painting = false
+
+function startPosition(){
+    painting = true
+}
+
+function endPosition(){
+    painting = false
+}
+
+function draw(e){
+    if(!painting) return
+    ctx.lineWidth = 10 
+    ctx.lineCap = ("round")
+    ctx.lineTo(e.clientX, e.clientY)
+    ctx.stroke
+}
+
+canvas.addEventListener("mouseDown", startPosition);
+canvas.addEventListener("mouseUp", endPosition);
+canvas.addEventListener("draw", draw);
+
+});
+
+
+
